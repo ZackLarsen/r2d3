f64b2735aa193ffd7f1c7a3f76568ac5465b02f4
ZackLarsen <zlarsen4@gmail.com>
2019-06-08 19:33:59 -0500

    updates

--- src/bubbles.js
+++ src/bubbles.js
@@ -1,8 +1,3 @@
-// !preview r2d3 data=c(0.3, 0.6, 0.8, 0.95, 0.40, 0.20)
-//
-// r2d3: https://rstudio.github.io/r2d3
-//
-
 var barHeight = Math.ceil(height / data.length);
 // !preview r2d3 data = read.csv("flare.csv"), d3_version = 4
 
